deb16460e8983fdbf439f8cbca152ae7bc85f966
mubin <mubin@kisanforum.org>
2020-07-05 22:03:52 +0530

    Updates

---
+++ precache-manifest.d53cd5c0e37bd0008a34a0448b0a67aa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e152d7a66694c5ba569aa0347637ed",
+    "url": "/ReactJS-Breaking-Bad-Cast/index.html"
+  },
+  {
+    "revision": "8eb1e86435dafbe1a1c3",
+    "url": "/ReactJS-Breaking-Bad-Cast/static/css/main.54c8eab3.chunk.css"
+  },
+  {
+    "revision": "063a7d9a38da3de85d6e",
+    "url": "/ReactJS-Breaking-Bad-Cast/static/js/2.813b259a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJS-Breaking-Bad-Cast/static/js/2.813b259a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb1e86435dafbe1a1c3",
+    "url": "/ReactJS-Breaking-Bad-Cast/static/js/main.7438e4ac.chunk.js"
+  },
+  {
+    "revision": "426615a4f2d4cf029a7f",
+    "url": "/ReactJS-Breaking-Bad-Cast/static/js/runtime-main.3453c898.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/ReactJS-Breaking-Bad-Cast/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/ReactJS-Breaking-Bad-Cast/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/ReactJS-Breaking-Bad-Cast/static/media/spinner.235b9b3e.gif"
+  }
+]);
